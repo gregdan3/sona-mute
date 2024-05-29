dae96f28ca7092043d38180bebaf099c3dfc4852
Gregory Danielson <gregory.danielson3@gmail.com>
2024-05-29 15:20:33 -0500

    mu

--- src/sonamute/__main__.py
+++ src/sonamute/__main__.py
@@ -1,56 +1,72 @@
 # STL
 import os
 import json
-import logging
 import argparse
-from collections.abc import Generator
+from collections import Counter
 
 # PDM
 from sonatoki.ilo import Ilo
-from sonatoki.Filters import (
-    Numerics,
-    Syllabic,
-    NimiLinku,
-    Alphabetic,
-    ProperName,
-    Punctuations,
-)
+from sonatoki.Configs import CorpusConfig
+from sonatoki.Filters import Numeric, Punctuation
 from sonatoki.Scorers import SoftScaling
-from sonatoki.Cleaners import ConsecutiveDuplicates
-from sonatoki.Tokenizers import sent_tokenize_tok, word_tokenize_tok
-from sonatoki.Preprocessors import URLs, DiscordEmotes, DiscordChannels, DiscordMentions
+from sonatoki.Cleaners import Lowercase, ConsecutiveDuplicates
+from sonatoki.Tokenizers import SentTokenizer, WordTokenizer
 
-F = "/home/gregdan3/communities/discord/ma-pona-pi-toki-pona/"
+# LOCAL
+from sonamute.db import MessageDB
+from sonamute.file_io import fetch_discord
 
+F = "/home/gregdan3/communities/discord/ma-pona-pi-toki-pona-05-06/"
 
-def fetch_discord_file() -> Generator[dict, None, None]:
-    for file in os.listdir(F):
-        with open(F + file, "r") as f:
-            content = json.loads(f.read())
-            yield content
-
-
-def fetch_discord_msg(content: dict) -> Generator[str, None, None]:
-    for m in content.get("messages", []):
-        yield m.get("content")
+KNOWN_BOTS = ["204255221017214977"]
 
 
 def main(argv: argparse.Namespace):
-    ilo = Ilo(
-        preprocessors=[URLs, DiscordEmotes, DiscordChannels, DiscordMentions],
-        ignoring_filters=[Numerics, Punctuations],
-        scoring_filters=[NimiLinku, Syllabic, ProperName, Alphabetic],
-        cleaners=[ConsecutiveDuplicates],
+    ilo = Ilo(**CorpusConfig)
+    # TODO: this is silly.
+    # i need a slightly differently cleaned output for counting purposes.
+    finalizer = Ilo(
+        preprocessors=[],
+        ignoring_filters=[Numeric, Punctuation],
+        scoring_filters=[],
+        cleaners=[Lowercase, ConsecutiveDuplicates],
         scorer=SoftScaling,
-        tokenizer=word_tokenize_tok,
-        passing_score=0.8,
+        passing_score=0.80,
+        word_tokenizer=WordTokenizer,
     )
-    ilo.logging_threshold = 0.799
 
-    for file in fetch_discord_file():
-        for msg in fetch_discord_msg(file):
-            for sent in sent_tokenize_tok(msg):
-                ilo.is_toki_pona(sent)
+    db = MessageDB("edgedb", "gU5wfFr5BPzlIgy2blnc38dJ", "localhost", 10700)
+
+    counter: dict[str, int] = Counter()
+    no_short: dict[str, int] = Counter()
+    for msg in fetch_discord(F):
+        msg = ilo.preprocess(msg)  # jump ahead to avoid urls, emotes, references
+        for sent in SentTokenizer.tokenize(msg):
+            processed, tokenized, filtered, cleaned, score, result = ilo._is_toki_pona(
+                sent
+            )
+            if not filtered:
+                continue
+            # if result and score < 0.86:
+            #     print(sent)
+
+            # if result and len(cleaned) >= 3:
+            if result:
+                # keep every token we discover, but normalized
+                discovered = finalizer.filter_tokens(tokenized)
+                discovered = finalizer.clean_tokens(discovered)
+                counter.update(discovered)
+                if len(cleaned) > 3:
+                    no_short.update(discovered)
+
+    sorted_counter = {
+        k: v for k, v in sorted(counter.items(), key=lambda i: i[1], reverse=True)
+    }
+    sorted_no_short = {
+        k: v for k, v in sorted(no_short.items(), key=lambda i: i[1], reverse=True)
+    }
+    print(json.dumps(sorted_counter, indent=2, ensure_ascii=False))
+    print(json.dumps(sorted_no_short, indent=2, ensure_ascii=False))
 
 
 if __name__ == "__main__":
@@ -75,9 +91,9 @@ if __name__ == "__main__":
     # )
     LOG_FORMAT = "%(message)s"
 
-    logging.basicConfig(format=LOG_FORMAT)
-    logger = logging.getLogger("sonatoki.ilo")
-    logger.setLevel(logging.DEBUG)
+    # logging.basicConfig(format=LOG_FORMAT)
+    # logger = logging.getLogger("sonatoki.ilo")
+    # logger.setLevel(logging.DEBUG)
 
     ARGV = parser.parse_args()
     main(ARGV)
