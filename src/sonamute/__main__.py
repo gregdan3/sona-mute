16dbf42e0196180020dbf5afd141e81242251735
Gregory Danielson <gregory.danielson3@gmail.com>
2024-06-26 23:43:12 -0500

    chore: note another ignorable container

--- src/sonamute/__main__.py
+++ src/sonamute/__main__.py
@@ -27,6 +27,7 @@ F = "/home/gregdan3/communities/discord/"
 IGNORED_CONTAINERS = {
     316066233755631616,  # ma pona/jaki
     786041291707777034,  # ma pona/ako
+    914305039764426772,  # ma pali/wikipesija
     1128714905932021821,  # ma musi/ako
     # The acrophobia bot is troublesome, because users trigger it with a phrase in toki pona.
     # Repeated uses push every word in "ilo o ako" up by >10,000 uses, changing their relative rankings even for o.
