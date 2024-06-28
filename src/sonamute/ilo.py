d06e5741af466a0f86c323e3bb0260001d470409
Gregory Danielson <gregory.danielson3@gmail.com>
2024-06-28 13:07:40 -0500

    chore: docs for choice to remove tokens from Ilo
    i really should offer a way to remove specific tokens from the
    dictionary

--- src/sonamute/ilo.py
+++ src/sonamute/ilo.py
@@ -4,3 +4,5 @@ from sonatoki.Configs import CorpusConfig
 
 ILO = Ilo(**CorpusConfig)
 ILO._Ilo__scoring_filters[0].tokens -= {"we", "i", "u", "ten", "to"}
+# These are all extremely infrequent in toki pona and make it more difficult to distinguish from English if counted in the dictionary.
+# We will still correctly discover them if they are among many other Toki Pona words.
