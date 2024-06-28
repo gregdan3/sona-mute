1f624943e3f460d56acf12787144f0338fc41cb2
Gregory Danielson <gregory.danielson3@gmail.com>
2024-06-28 17:16:00 -0500

    feat: Small util function for generating consecutive dates

--- src/sonamute/utils.py
+++ src/sonamute/utils.py
@@ -1,5 +1,7 @@
 # STL
 import os
+from typing import Generator
+from datetime import date, datetime, timedelta
 
 # PDM
 import dotenv
@@ -18,3 +20,17 @@ def load_envvar(envvar: str, default: str | None = None) -> str:
     if default is not None:
         return default
     raise EnvironmentError(f"No {envvar} found in environment!")
+
+
+def dates_in_range(start: datetime, end: datetime) -> Generator[datetime, None, None]:
+    """
+    Provide datetimes rounded to every `date` from `start` to `end`, plus a day for coverage.
+    """
+    start = datetime.combine(start, datetime.min.time(), tzinfo=start.tzinfo)
+    end = datetime.combine(end, datetime.min.time(), tzinfo=end.tzinfo) + timedelta(
+        days=1
+    )
+
+    while start <= end:
+        yield start
+        start += timedelta(days=1)
