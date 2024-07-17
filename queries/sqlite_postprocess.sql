b2480a4aba6efd4386917001d0aaba5cd302f3a0
Gregory Danielson <gregory.danielson3@gmail.com>
2024-07-17 07:41:14 -0500

    feat: further clean sqlite data
    somewhat for presentational purposes; we omit the last month since it's
    incomplete

--- queries/sqlite_postprocess.sql
+++ queries/sqlite_postprocess.sql
@@ -1,3 +1,5 @@
+/* Remove words/phrases below a certain number of occurrences, here 40
+* Partly for anonymity, but mostly for Google's reasons: the data is massive if you don't do this */
 WITH
   summed_occurrences AS (
     SELECT
@@ -27,6 +29,28 @@ WHERE
       phrases_to_delete
   );
 
+/* NOTE: Deletes the last month from the data
+* This is presumed to be safe due to the data being incomplete
+* May not be safe if data was collected on the last day of the month exactly */
+DELETE FROM frequency
+WHERE
+  day IN (
+    SELECT
+      MAX(day)
+    FROM
+      frequency
+  );
+
+DELETE FROM total
+WHERE
+  day IN (
+    SELECT
+      MAX(day)
+    FROM
+      total
+  );
+
+/* Most of disk space savings are here */
 DELETE FROM phrase
 WHERE
   id NOT IN (
@@ -40,6 +64,7 @@ CREATE INDEX FreqCovering on frequency (phrase_id, min_sent_len, day, occurrence
 
 CREATE INDEX TotalCovering on total (phrase_len, min_sent_len, day, occurrences);
 
+/* For HttpVFS performance */
 pragma journal_mode = delete;
 
 pragma page_size = 1024;
