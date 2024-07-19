f02a872ad820adb5411421f4d3f7683f6a65a801
Gregory Danielson <gregory.danielson3@gmail.com>
2024-07-18 20:38:46 -0500

    fix: re-order deleting max and deleting low-occurrence words

--- queries/sqlite_postprocess.sql
+++ queries/sqlite_postprocess.sql
@@ -1,59 +1,3 @@
-/* Remove words/phrases below a certain number of occurrences, here 40
-* Partly for anonymity, but mostly for Google's reasons: the data is massive if you don't do this */
-WITH
-  summed_occurrences AS (
-    SELECT
-      f.phrase_id,
-      SUM(f.occurrences) AS total_occurrences
-    FROM
-      frequency f
-      JOIN phrase p ON f.phrase_id = p.id
-    WHERE
-      /* yes this is nonsense */
-      (
-        f.min_sent_len = 1
-        AND p.phrase_len = 1
-      )
-      OR (
-        f.min_sent_len = 2
-        AND p.phrase_len = 2
-      )
-      OR (
-        f.min_sent_len = 3
-        AND p.phrase_len = 3
-      )
-      OR (
-        f.min_sent_len = 4
-        AND p.phrase_len = 4
-      )
-      OR (
-        f.min_sent_len = 5
-        AND p.phrase_len = 5
-      )
-      OR (
-        f.min_sent_len = 6
-        AND p.phrase_len = 6
-      )
-    GROUP BY
-      phrase_id
-  ),
-  phrases_to_delete AS (
-    SELECT
-      phrase_id
-    FROM
-      summed_occurrences
-    WHERE
-      total_occurrences < 40
-  )
-DELETE FROM frequency
-WHERE
-  phrase_id IN (
-    SELECT
-      phrase_id
-    FROM
-      phrases_to_delete
-  );
-
 /* NOTE: Deletes the last month from the data
 * This is presumed to be safe due to the data being incomplete
 * May not be safe if data was collected on the last day of the month exactly */
@@ -73,6 +17,62 @@ WHERE
       MAX(day)
     FROM
       total
+  );
+
+/* Remove words/phrases below a certain number of occurrences, here 40
+* Partly for anonymity, but mostly for Google's reasons: the data is massive if you don't do this */
+WITH
+  summed_occurrences AS (
+    SELECT
+      f.phrase_id,
+      SUM(f.occurrences) AS total_occurrences
+    FROM
+      frequency f
+      JOIN phrase p ON f.phrase_id = p.id
+    WHERE
+      /* yes this is nonsense */
+      (
+        f.min_sent_len = 1
+        AND len = 1
+      )
+      OR (
+        f.min_sent_len = 2
+        AND len = 2
+      )
+      OR (
+        f.min_sent_len = 3
+        AND len = 3
+      )
+      OR (
+        f.min_sent_len = 4
+        AND len = 4
+      )
+      OR (
+        f.min_sent_len = 5
+        AND len = 5
+      )
+      OR (
+        f.min_sent_len = 6
+        AND len = 6
+      )
+    GROUP BY
+      phrase_id
+  ),
+  phrases_to_delete AS (
+    SELECT
+      phrase_id
+    FROM
+      summed_occurrences
+    WHERE
+      total_occurrences < 40
+  )
+DELETE FROM frequency
+WHERE
+  phrase_id IN (
+    SELECT
+      phrase_id
+    FROM
+      phrases_to_delete
   );
 
 /* Most of disk space savings are here */
