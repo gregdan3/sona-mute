cf5ec9c8d9229d0941cd265664ae39f629f9d82f
Gregory Danielson <gregory.danielson3@gmail.com>
2025-01-03 16:25:11 -0600

    chore: import ignores from main head

--- src/sonamute/constants.py
+++ src/sonamute/constants.py
@@ -6,6 +6,10 @@ IGNORED_CONTAINERS = {
     786041291707777034,  # ma pona/ako
     914305039764426772,  # ma pali/wikipesija
     1128714905932021821,  # ma musi/ako
+    # NOTE: the below two are not in my dump, but they're good examples of
+    # things to omit: log channels
+    759969963992940585,  # ma pona/jaki lawa
+    842196795756249118,  # ma pona/invite lawa
     # The acrophobia bot is troublesome, because users trigger it with a phrase in toki pona.
     # Repeated uses push every word in "ilo o ako" up by >10,000 uses, changing their relative rankings even for o.
 }
