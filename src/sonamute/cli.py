6f7bfe3b1ba61a777d8e3e16237d9056a0169368
Gregory Danielson <gregory.danielson3@gmail.com>
2025-09-27 19:58:01 -0500

    chore: remove todo

--- src/sonamute/cli.py
+++ src/sonamute/cli.py
@@ -1,10 +1,3 @@
-# TODO: replace the behavior of main (and gen_sqlite) with some prompts here that
-# - let you fetch from various configured sources, specifying a dir
-# - let you transform to frequency in the db
-# - let you generate a sqlite file
-# all as consecutive options you can pick, or
-# - separately, let you postprocess pluralkit data, when that's implemented
-
 # STL
 import os
 import sys
